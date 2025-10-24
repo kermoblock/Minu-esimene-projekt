d9967483e4338b264e608e73e7045aaa6fad82ee
Opilane TTHK <opilane@tthk.ee>
2025-10-24 09:03:15 +0300

    ListatudProjekt

---
+++ Uusprojekt/Projekt/Projekt/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
